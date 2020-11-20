96d3259f9eea0a9e46c1e7ff1c628cf4138286dd
Juliya <prakhova_yuliya@ukr.net>
2020-11-20 19:56:22 +0200

    -

--- main.js
+++ main.js
@@ -60,7 +60,6 @@ function moveTetroDown() {
         for (var y = playfield.length - 1; y >= 0; y--) {
             for (var x = 0; x < playfield[y].length; x++) {
                 if (playfield[y][x] === 1) {
-                    //console.log(playfield[y + 1]);
                     playfield[y + 1][x] = 1;
                     playfield[y][x] = 0;
                 }
@@ -71,6 +70,34 @@ function moveTetroDown() {
     }
 }
 
+//Двигаем фигурку влево
+function canTetroMoveLeft() {
+    for (var y = 0; y < playfield.length; y++) {
+        for (var x = 0; x < playfield[y].length; x++) {
+            if (playfield[y][x] === 1) {
+                if (x === 0) {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return true;
+}
+
+function moveTetroLeft() {
+    if (canTetroMoveLeft()) {
+        for (var y = playfield.length - 1; y >= 0; y--) {
+            for (var x = 0; x < playfield[y].length; x++) {
+                if (playfield[y][x] === 1) {
+                    playfield[y][x - 1] = 1;
+                    playfield[y][x] = 0;
+                }
+            }
+        }
+    } 
+}
+
 function fixTetro() {
     for (var y = 0; y < playfield.length; y++) {
         for (var x = 0; x < playfield[y].length; x++) {
@@ -79,9 +106,25 @@ function fixTetro() {
             }
         }
     }
+
+    playfield[0] = [0, 0, 0, 0, 1, 0, 0, 0, 0, 0];
+    playfield[1] = [0, 0, 0, 1, 1, 1, 0, 0, 0, 0];
 }
 
 draw();
+
+document.onkeydown = function (e) { 
+    console.log(e);
+    if (e.code === 37) {
+        //двигаем фигурку вправо
+        moveTetroLeft();
+        } else if (e.code === 39) {
+            //двигаем фигурку вправо
+        }
+        else if (e.code === 40) {
+            //ускоряемся
+    }
+};  
 
 function startGame() {
     moveTetroDown();
@@ -91,3 +134,16 @@ function startGame() {
 
 setTimeout(startGame, gameSpeed);
 
+var dispatchForCode = function(event, callback) {
+    var code;
+  
+    if (event.key !== undefined) {
+      code = event.key;
+    } else if (event.keyIdentifier !== undefined) {
+      code = event.keyIdentifier;
+    } else if (event.keyCode !== undefined) {
+      code = event.keyCode;
+    }
+  
+    callback(code);
+  };
