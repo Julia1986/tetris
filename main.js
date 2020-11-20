f4d8f9e7f0f94f1e7c0daeaa4bc83d5757d42e84
Juliya <prakhova_yuliya@ukr.net>
2020-11-20 11:53:08 +0200

    moving figures

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ var main = document.querySelector(".main");
 var playfield = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -23,10 +23,33 @@ var playfield = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
-var mainInnerHTML = "";
-for (var y = 0; y < playfield.length; y++) {
-    for (var x = 0; x < playfield[y].length; x++) {
-        mainInnerHTML += '<div class="cell"></div>';
+function draw() {
+    var mainInnerHTML = "";
+        for (var y = 0; y < playfield.length; y++) {
+            for (var x = 0; x < playfield[y].length; x++) {
+                if (playfield[y][x] === 1) {
+                    mainInnerHTML += '<div class="cell movingCell"></div>';
+                } else {
+                    mainInnerHTML += '<div class="cell"></div>';
+                }
+            }
+        }
+    main.innerHTML = mainInnerHTML;
+}
+
+function moveTetroDown() {
+    for (var y = playfield.length - 1; y >= 0; y--) {
+        for (var x = 0; x < playfield[y].length; x++) {
+            if (playfield[y][x] === 1) {
+                console.log(playfield[y]);
+                playfield[y + 1][x] = 1;
+                playfield[y][x] = 0;
+            }
+        }
     }
 }
-main.innerHTML = mainInnerHTML;
+
+draw();
+
+moveTetroDown();
+draw();
