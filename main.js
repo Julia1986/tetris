1283a31567d3349cd2091f5a06c09e04981a6605
Juliya <prakhova_yuliya@ukr.net>
2020-11-22 19:08:42 +0200

    new changes

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ function addActiveTetro() {
     removePrevActiveTetro();
     for (var y = 0; y < activeTetro.shape.length; y++) {
         for (var x = 0; x < activeTetro.shape[y].length; x++) {
-            if (activeTetro.shape[y][x]) {
+            if (activeTetro.shape[y][x] === 1) {
                 playfield[activeTetro.y + y][activeTetro.x + x] = activeTetro.shape[y][x];
             }
         }
@@ -74,97 +74,14 @@ function addActiveTetro() {
 function hasCollisions() {
     for (var y = 0; y < activeTetro.shape.length; y++) {
         for (var x = 0; x < activeTetro.shape[y].length; x++) {
-            if (activeTetro.shape[y][x] && playfield[activeTetro.y + y] === undefined) {
+            if (activeTetro.shape[y][x] && (playfield[activeTetro.y + y] === undefined ||
+                playfield[activeTetro.y + y][activeTetro.x + x] === undefined)
+                ) {
                 return true;
             }
         }
     } 
     return false;
-}
-
-function canTetroMoveDown() {
-    for (var y = 0; y < playfield.length; y++) {
-        for (var x = 0; x < playfield[y].length; x++) {
-            if (playfield[y][x] === 1) {
-                if (y === playfield.length - 1 || playfield[y + 1][x] === 2) {
-                    return false;
-                }
-            }
-        }
-    }
-
-    return true;
-}
-
-function moveTetroDown() {
-    if(canTetroMoveDown()) {
-        for (var y = playfield.length - 1; y >= 0; y--) {
-            for (var x = 0; x < playfield[y].length; x++) {
-                if (playfield[y][x] === 1) {
-                    playfield[y + 1][x] = 1;
-                    playfield[y][x] = 0;
-                }
-            }
-        }
-    } else {
-        fixTetro();
-    }
-}
-
-//Двигаем фигурку влево
-function canTetroMoveLeft() {
-    for (var y = 0; y < playfield.length; y++) {
-        for (var x = 0; x < playfield[y].length; x++) {
-            if (playfield[y][x] === 1) {
-                if (x === 0 || playfield[y][x-1] === 2) {
-                    return false;
-                }
-            }
-        }
-    }
-
-    return true;
-}
-
-function moveTetroLeft() {
-    if (canTetroMoveLeft()) {
-        for (var y = playfield.length - 1; y >= 0; y--) {
-            for (var x = 0; x < playfield[y].length; x++) {
-                if (playfield[y][x] === 1) {
-                    playfield[y][x - 1] = 1;
-                    playfield[y][x] = 0;
-                }
-            }
-        }
-    } 
-}
-
-//Двигаем фигурку вправо
-function canTetroMoveRight() {
-    for (var y = 0; y < playfield.length; y++) {
-        for (var x = 0; x < playfield[y].length; x++) {
-            if (playfield[y][x] === 1) {
-                if (x === 9 || playfield[y][x + 1] ===2) { //вместо девятки можно playfield[0].length - 1
-                    return false;
-                }
-            }
-        }
-    }
-
-    return true;
-}
-
-function moveTetroRight() {
-    if (canTetroMoveRight()) {
-        for (var y = playfield.length - 1; y >= 0; y--) {
-            for (var x = 9; x >= 0; x--) {
-                if (playfield[y][x] === 1) {
-                    playfield[y][x + 1] = 1;
-                    playfield[y][x] = 0;
-                }
-            }
-        }
-    } 
 }
 
 function removeFullLines() {
@@ -192,23 +109,25 @@ function fixTetro() {
             }
         }
     }
-
-    removeFullLines();
-
-    playfield[0] = [0, 0, 0, 0, 1, 1, 0, 0, 0, 0];
-    playfield[1] = [0, 0, 0, 0, 1, 1, 0, 0, 0, 0];
 }
 
 document.onkeydown = function(e) { 
     if (e.code === "ArrowLeft") {
             activeTetro.x -= 1; //moveTetroLeft();
+            if (hasCollisions()) {
+                activeTetro.x += 1;
+            }
         } else if (e.code === "ArrowRight") {
             activeTetro.x += 1; //moveTetroRight();
-        }
-        else if (e.code === "ArrowDown") {
+            if (hasCollisions()) {
+                activeTetro.x -= 1;
+            }
+        } else if (e.code === "ArrowDown") {
             activeTetro.y += 1; //moveTetroDown();
             if (hasCollisions()) {
                 activeTetro.y -= 1;
+                fixTetro();
+                activeTetro.y = 0;
             }
     }
 
